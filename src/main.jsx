2dbd8c5a7df9e7f8ac43afb6845a34689ea53928
aleeshakhalid911 <alishakhalid911@gmail.com>
2025-11-12 20:05:54 +0500

    Code deployed.

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-
-// Context Providers
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { UserProvider } from "./context/UserContext.jsx";
-import { TicketProvider } from "./context/TicketContext.jsx";
-import { BookingProvider } from "./context/BookingContext.jsx";
-
-import "./index.css"; // Tailwind or CSS
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <UserProvider>
-        <TicketProvider>
-          <BookingProvider>
-            <App />
-          </BookingProvider>
-        </TicketProvider>
-      </UserProvider>
-    </AuthProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
